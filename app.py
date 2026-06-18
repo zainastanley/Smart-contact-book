c44044e82b24c0db5fd5febc32b3149316393164
zainastanley <zainarosestanley@gmail.com>
2026-06-18 19:05:25 +0530

    Added name and phone fields with a save button

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import tkinter as tk
+
 root=tk.Tk()
+root.title("Smart Contact Book")
+root.geometry("500x500")
+
+name_label=tk.Label(root, text="Contact Name:")
+name_label.pack(pady=5)
+
+name_entry=tk.Entry(root, width=30)
+name_entry.pack(pady=5)
+
+phone_label=tk.Label(root, text="Phone Number")
+phone_label.pack(pady=5)
+
+phone_entry=tk.Entry(root, width=30)
+phone_entry.pack(pady=5)
+
+save_button=tk.Button(root, text="Save")
+save_button.pack(pady=15)
+
 root.mainloop()
